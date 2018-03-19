9f3c15ffeb34a4e298caeee4cf846ac7672960c8
Alex Justesen <alex@alexjustesen.com>
2018-03-19 18:31:20 -0400

    new add action for register shortcode init

--- includes/class-custom-untappd-feeds.php
+++ includes/class-custom-untappd-feeds.php
@@ -175,10 +175,10 @@ class Custom_Untappd_Feeds {
 
 		$plugin_public = new Custom_Untappd_Feeds_Public( $this->get_plugin_name(), $this->get_version() );
         
-        $this->loader->add_action( 'add_shotcode', $plugin_public, 'add_shortcodes' );
-        
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+        
+        $this->loader->add_action( 'init', $plugin_public, 'register_shortcodes' );
 
 	}
 
